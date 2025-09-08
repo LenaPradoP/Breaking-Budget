358c17149e639acae6318bdbab82b8e17b7a1eaa
annaszczek-lang <anna.szczek@travelperk.com>
2025-09-08 15:19:44 +0200

    fix merge conflit

--- users/views/__init__.py
+++ users/views/__init__.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-from .LoginView import login_user, home
-from .DetailView import user_detail
-=======
 from .LoginView import login_user, home 
 from .CreateView import new_user, create_user
->>>>>>> main
+
