b9380e415fd196e1a9282744be189016281befc7
joaquinvillaronga <joaquin.villaronga@travelperk.com>
2025-09-08 10:43:38 +0200

    check pull request

--- users/views.py
+++ users/views.py
@@ -22,7 +22,7 @@ def login_view(request):
             form.add_error(None, "Invalid username or password")
     else:
         form = LoginForm()
-    return render(request, "users/registration/login.html", {"form": form})
+    return render(request, "users/login.html", {"form": form})
 
 def home(request):
     return render(request, "users/home.html")
