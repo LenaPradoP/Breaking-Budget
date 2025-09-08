9bce3471061961144f173c1ae0790d0484a4f8e4
joaquinvillaronga <joaquin.villaronga@travelperk.com>
2025-09-08 11:42:20 +0200

    initial commit

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
-# Used by your manual login view
+# Used by the web login view
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150)
     password = forms.CharField(widget=forms.PasswordInput)
