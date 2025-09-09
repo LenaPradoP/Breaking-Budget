15421245800435633c2313a211f75e579ab797d4
annaszczek-lang <anna.szczek@travelperk.com>
2025-09-09 09:47:39 +0200

    added feature admin: delete an user profile

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LoginView, DetailView, CreateView
+from .views import LoginView, DetailView, CreateView, DeleteUser
 
 app_name = "users"
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path("<int:pk>/", DetailView.user_detail, name="user_detail"),
     path("new/", CreateView.new_user, name="new_user"),
     path("create/", CreateView.create_user, name="create_user"),
+    path("<int:pk>/", DeleteUser.delete_user, name="delete_user"),
+
 
 ]
