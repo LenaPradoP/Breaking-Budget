06ee9f0ecf13a0a706f1bd359a453f0e657dd210
joaquinvillaronga <joaquin.villaronga@travelperk.com>
2025-09-10 10:45:57 +0200

    Admin Review Expenses

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ListView, DeleteView
+from .views import ListView, DeleteView, ReviewExpense
 
 app_name = "expenses"   
 
 urlpatterns = [
     path("", ListView.list_expenses, name="list_expenses"),
     path("<int:pk>/delete/", DeleteView.delete_expense, name="delete_expense"),
+    path("status/<int:pk>/", ReviewExpense.update_expense_status, name="update_expense_status"),
 ]
